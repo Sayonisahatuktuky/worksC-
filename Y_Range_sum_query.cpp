f74171ffed6d5d85514d3db188a3d6e1c20de31b
Shayoni Tuktuky <124164802+Sayonisahatuktuky@users.noreply.github.com>
2024-06-29 12:32:01 +0600

    Add files via upload

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ int N,q;
+ cin>>N>>q;
+ int A[N];
+ for(int i=0;i<N;i++){
+    cin>>A[i];
+ }
+ int pre[N];
+ pre[0]=A[0];
+  for(int i=1;i<N;i++){
+    pre[i]=A[i]+pre[i-1];
+ }
+  for(int i=0;i<N;i++){
+    cout<<pre[i]<<" ";
+ }
+ while(q--){
+    int l,r; 
+    cin>>l>>r;
+    l--;
+    r--;
+    int sum=0;
+    // if(l==0)sum=pre[N];
+    // else sum=pre[N]-pre[l-1];
+ }
+
+  return 0;
+}
