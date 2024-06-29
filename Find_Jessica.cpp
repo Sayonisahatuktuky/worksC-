5bf2bb19a23f591c0507a2cd4f52c97480c094a6
Shayoni Tuktuky <124164802+Sayonisahatuktuky@users.noreply.github.com>
2024-06-29 12:27:27 +0600

    Add files via upload

---
+++ Find_Jessica.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string S;
+    getline(cin, S);  
+    stringstream ss(S);
+    string word;
+    bool found = false;
+
+    while (ss >> word) {
+        if (word == "Jessica") {
+            found = true;
+        }
+    }
+
+    if (found) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
