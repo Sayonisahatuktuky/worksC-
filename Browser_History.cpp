b1779f033401267e7bcb5d43391bc13120a39890
Shayoni Tuktuky <124164802+Sayonisahatuktuky@users.noreply.github.com>
2024-07-07 18:18:43 +0600

    Update Browser_History.cpp

--- Browser_History.cpp
+++ Browser_History.cpp
@@ -1,80 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-public:
-    string val;
-    Node* next;
-    Node* prev;
-    Node(string val) {
-        this->val = val;
-        this->next = NULL;
-        this->prev = NULL;
-    }
-};
-
-void insert_at_tail(Node*& head, Node*& tail, string v) {
-    Node* newNode = new Node(v);
-    if (head == NULL) {
-        head = newNode;
-        tail = newNode;
-        return;
-    }
-    tail->next = newNode;
-    newNode->prev = tail;
-    tail = newNode;
-}
-
-Node* find(Node* head,string& address) {
-    Node* temp = head;
-    while (temp != NULL) {
-        if (temp->val == address) {
-            return temp;
-        }
-        temp = temp->next;
-    }
-    return NULL;
-}
-
 int main() {
-    Node* head = NULL;
-    Node* tail = NULL;
-    string val;
- while(true){
-     cin>>val;
- if(val=="end"){
-    break;
-   }
-        insert_at_tail(head, tail, val);
- }
+    list<string> mylist;
+    string value;
+
+    while (true) {
+        cin>>value;
+        if(value=="end"){
+            break;
+        }
+        mylist.push_back(value);
+    }
     
     int Q;
     cin >> Q;
-    Node* current = head;
-    string command, address;
+    string cmd, address;
+    auto it = mylist.begin();
     
-    for (int i = 0; i < Q; ++i) {
-        cin >> command;
-        if (command == "visit") {
+    for (int i = 0; i < Q; i++) {
+        cin >> cmd;
+        if (cmd == "visit") {
             cin >> address;
-            Node* visit = find(head, address);
-            if (visit) {
-                current = visit;
-                cout << current->val << endl;
+            auto _it = find(mylist.begin(), mylist.end(), address);
+            if (_it != mylist.end()) {
+                it = _it;
+                cout << *it << endl;
             } else {
                 cout << "Not Available" << endl;
             }
-        } else if (command == "next") {
-            if (current->next) {
-                current = current->next;
-                cout << current->val << endl;
+        } else if (cmd == "next") {
+            if (next(it) != mylist.end()) {
+                it++;
+                cout << *it << endl;
             } else {
                 cout << "Not Available" << endl;
             }
-        } else if (command == "prev") {
-            if (current->prev) {
-                current = current->prev;
-                cout << current->val << endl;
+        } else if (cmd == "prev") {
+            if (it != mylist.begin()) {
+                it--;
+                cout << *it << endl;
             } else {
                 cout << "Not Available" << endl;
             }
